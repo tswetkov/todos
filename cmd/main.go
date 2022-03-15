c7d4f08c1220fa704edb16a101a117954e57a459
Roman Tsvetkov <tsvetkovv22@gmail.com>
2022-03-15 22:21:09 +0300

    feat: added service & repo

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,17 @@ import (
 
 	"github.com/tswetkov/todos"
 	"github.com/tswetkov/todos/pkg/handler"
+	"github.com/tswetkov/todos/pkg/repository"
+	"github.com/tswetkov/todos/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	server := new(todos.Server)
 
 	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
-		log.Fatalf("Error: %s", err.Error())
+		log.Fatalf("Main error: %s", err.Error())
 	}
 }
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,8 +1,17 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tswetkov/todos/pkg/service"
+)
 
 type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
+
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
