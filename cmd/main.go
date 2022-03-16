5686dca91d09b4dc406ea9a76d3c8744009614b4
Roman Tsvetkov <tsvetkovv22@gmail.com>
2022-03-16 19:58:48 +0300

    feat: added migrations

--- cmd/main.go
+++ cmd/main.go
@@ -3,19 +3,31 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/viper"
 	"github.com/tswetkov/todos"
 	"github.com/tswetkov/todos/pkg/handler"
 	"github.com/tswetkov/todos/pkg/repository"
 	"github.com/tswetkov/todos/pkg/service"
 )
 
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
+}
+
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Initializing error!")
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	server := new(todos.Server)
 
-	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Main error: %s", err.Error())
 	}
 }
---
+++ schema/000001_init.down.sql
@@ -0,0 +1,9 @@
+DROP TABLE lists_items;
+
+DROP TABLE users_lists;
+
+DROP TABLE todo_items;
+
+DROP TABLE users;
+
+DROP TABLE todo_lists;
